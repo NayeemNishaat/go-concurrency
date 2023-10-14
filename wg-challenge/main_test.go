99e51d83f054a9063b3fda01324c08ac31fab248
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-10-14 17:45:08 +0600

    updated tests

--- wg-challenge/main_test.go
+++ wg-challenge/main_test.go
@@ -2,12 +2,54 @@ package main
 
 import (
 	"io"
+	"log"
 	"os"
 	"strings"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	log.Println("Do stuff BEFORE the tests!")
+	exitVal := m.Run()
+	log.Println("Do stuff AFTER the tests!")
+
+	os.Exit(exitVal)
+}
+
+func TestUpdateMessage(t *testing.T) {
+	myMsg := "my text"
+
+	wg.Add(1)
+	go updateMessage(myMsg)
+	wg.Wait()
+
+	if msg != myMsg {
+		t.Errorf("updateMessage Failed")
+	}
+}
+
 func TestPrintMessage(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	printMessage()
+
+	_ = w.Close()
+
+	data, _ := io.ReadAll(r)
+	dataStr := string(data)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(dataStr, msg) {
+		t.Errorf("printMessage Failed")
+	}
+
+}
+
+func TestPrintUpdateMessage(t *testing.T) {
 	stdOut := os.Stdout
 
 	r, w, _ := os.Pipe()
@@ -36,5 +78,33 @@ func TestPrintMessage(t *testing.T) {
 	if msg != strings.Trim(dataStr, "\n") {
 		// Note: Multiple chars can be provided strings.Trim(dataStr, "\n -")
 		t.Errorf("printMessage Failed")
+	}
+}
+
+func TestMainFunc(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+
+	data, _ := io.ReadAll(r)
+	dataStr := string(data)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(dataStr, "Hello, universe!") {
+		t.Errorf("'Hello, universe!' not found!")
+	}
+
+	if !strings.Contains(dataStr, "Hello, cosmos!") {
+		t.Errorf("'Hello, cosmos!' not found!")
+	}
+
+	if !strings.Contains(dataStr, "Hello, world!") {
+		t.Errorf("'Hello, world!' not found!")
 	}
 }
