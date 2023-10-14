3c88517b660a65e7cc400c8d8199555f4bc369a2
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-10-14 13:58:41 +0600

    tested print something

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+)
+
+func TestPrintSomething(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+
+	os.Stdout = w
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go printSomething("Epsilon", &wg)
+
+	wg.Wait()
+
+	_ = w.Close()
+
+	result, _ := io.ReadAll(r)
+	output := string(result)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(output, "Epsilon") {
+		t.Errorf("Expected Epislon but not found!")
+	}
+}
