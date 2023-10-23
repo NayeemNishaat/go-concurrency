89ffe427c491f8130c702d3217d1b55a5285a0fe
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-10-23 12:13:45 +0600

    buffered chan

--- slp-brb/main.go
+++ slp-brb/main.go
@@ -5,41 +5,26 @@ import (
 	"time"
 )
 
-func server1(ch chan string) {
-	for {
-		time.Sleep(5 * time.Second)
-		ch <- "From server 1."
-	}
-}
+func listen(ch chan int) {
+	for { // Important: If infinite for loop is not used then this go routine dies after it reaches to the end of function. An infinite for loop makes the go routine to loop again, wait for input from channel.
+		i := <-ch
+		fmt.Println("Got", i)
 
-func server2(ch chan string) {
-	for {
-		time.Sleep(2 * time.Second)
-		ch <- "From server 2."
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func main() {
-	fmt.Println("Select with channels.")
+	ch := make(chan int, 10) // Note: Now channel capacity is 10 by default 1.
 
-	channel1 := make(chan string)
-	channel2 := make(chan string)
+	go listen(ch)
 
-	go server1(channel1)
-	go server2(channel2)
-
-	for {
-		select {
-		case s1 := <-channel1:
-			fmt.Println("Case 1:", s1)
-		case s2 := <-channel1: // Note: If multiple cases of listening from same channel go will choose one randomly
-			fmt.Println("Case 2:", s2)
-		case s3 := <-channel2:
-			fmt.Println("Case 3:", s3)
-		case s4 := <-channel2:
-			fmt.Println("Case 4:", s4)
-			// default:
-			// Remark: Avoid Deadlock
-		}
+	for i := 0; i < 100; i++ {
+		fmt.Println("Sending", i)
+		ch <- i
+		fmt.Println("Sent", i)
 	}
+
+	fmt.Println("Done")
+	close(ch)
 }
