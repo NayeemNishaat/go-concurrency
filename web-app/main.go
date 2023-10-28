aa5c8eee4eb5274042c555a5fc004931bbd98536
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-10-28 22:34:34 +0600

    updated

--- web-app/main.go
+++ web-app/main.go
@@ -1,12 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
+	"time"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
 )
 
+var envFile map[string]string
+
 func main() {
-	envFile, _ := godotenv.Read(".env")
-	fmt.Println(envFile["PORT"])
+	envFile, _ = godotenv.Read(".env")
+	db := initDB()
+	db.Ping()
 }
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("Can't connect to the DB.")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	count := 0
+
+	dsn := envFile["DSN"]
+
+	for {
+		conn, err := openDB(dsn)
+
+		if err != nil {
+			log.Println("DB is not yet ready.")
+		} else {
+			log.Print("Connected to DB.")
+			return conn
+		}
+
+		if count > 5 {
+			return nil
+		}
+
+		log.Print("Backing off for 1 second.")
+		time.Sleep(1 * time.Second)
+
+		count++
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// brew services start postgresql@16
+// /opt/homebrew/opt/postgresql@16/bin/postgres -D /opt/homebrew/var/postgresql@16
