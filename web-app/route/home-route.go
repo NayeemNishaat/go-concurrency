f8665d8cb27a22b18f424a29332d44ceff3c663c
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-01 15:16:30 +0600

    handled unrecognized routes

---
+++ web-app/route/home-route.go
@@ -0,0 +1,11 @@
+package route
+
+import (
+	"net/http"
+	"web/controller"
+	"web/middleware"
+)
+
+func HomeRoute(m *http.ServeMux, globalMiddlewars ...middleware.Middleware) {
+	m.HandleFunc("/", middleware.Chain(controller.HomePage, globalMiddlewars, middleware.Method("GET"), middleware.Token()))
+}
--- web-app/route/index-route.go
+++ web-app/route/index-route.go
@@ -7,7 +7,7 @@ import (
 )
 
 func InitRouter(m *http.ServeMux) {
-	HelloRoute(m, middleware.Logging())
+	HomeRoute(m, middleware.Logging())
 
-	m.HandleFunc("/", middleware.Chain(func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "This Route Is Unavailable!") }, []middleware.Middleware{middleware.Logging()}))
+	m.HandleFunc("/404", middleware.Chain(func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "This Route Is Unavailable!\n") }, []middleware.Middleware{}))
 }
