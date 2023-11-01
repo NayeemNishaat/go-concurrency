13120433e7388e88744d9dd8433d08e2bb1ad296
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-01 17:38:02 +0600

    implemented method manager

--- web-app/controller/auth-controller.go
+++ web-app/controller/auth-controller.go
@@ -5,6 +5,45 @@ import (
 	"web/templates"
 )
 
+func RegisterMethodManager(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		RegisterPage(w, r)
+		return
+	case http.MethodPost:
+		Register(w, r)
+		return
+	case http.MethodPut:
+		// Update an existing record.
+	case http.MethodDelete:
+		// Remove the record.
+	case http.MethodPatch:
+		// Update an existing record partially.
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func LoginMethodManager(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		LoginPage(w, r)
+		// middleware.Chain(LoginPage, []middleware.Middleware{middleware.Logging()}).ServeHTTP(w, r)
+		return
+	case http.MethodPost:
+		Login(w, r)
+		return
+	case http.MethodPut:
+		// Update an existing record.
+	case http.MethodDelete:
+		// Remove the record.
+	case http.MethodPatch:
+		// Update an existing record partially.
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func RegisterPage(w http.ResponseWriter, r *http.Request) {
 	templates.Render(w, r, "register.page.gohtml", nil)
 }
